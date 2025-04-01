24b35f50f2429100165aa205c749e15171df654f
skosito <skostic9242@gmail.com>
2025-04-01 16:58:27 +0100

    boilerplate for universal token with localnet (#5)

---
+++ token/scripts/localnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+set -o pipefail
+
+npx hardhat localnet --exit-on-error --skip solana,sui & sleep 25
+echo -e "\n🚀 Compiling contracts..."
+npx hardhat compile --force --quiet
+
+GATEWAY_ZETACHAIN=$(jq -r '.addresses[] | select(.type=="gatewayZEVM" and .chain=="zetachain") | .address' localnet.json)
+UNISWAP_ROUTER=$(jq -r '.addresses[] | select(.type=="uniswapRouterInstance" and .chain=="zetachain") | .address' localnet.json)
+
+CONTRACT_ZETACHAIN=$(npx hardhat token:deploy --name ZetaIdxUniversalToken --network localhost --gateway "$GATEWAY_ZETACHAIN" --uniswap-router "$UNISWAP_ROUTER" --json | jq -r '.contractAddress')
+echo -e "\n🚀 Deployed ZetaIdxUniversalToken contract on ZetaChain: $CONTRACT_ZETACHAIN"
