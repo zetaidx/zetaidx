954b1bdc19674da9b6a1f1a8b179adfbe943e2c3
skosito <skostic9242@gmail.com>
2025-04-02 22:22:57 +0200

    omit hardhat testnet network if private key not set

--- token/hardhat.config.ts
+++ token/hardhat.config.ts
@@ -12,21 +12,26 @@ import { HardhatUserConfig } from "hardhat/config";
 
 dotenv.config();
 
+const networks = {
+  hardhat: {
+    accounts: {
+      mnemonic: "test test test test test test test test test test test junk",
+    },
+    chainId: 1337,
+  },
+};
+
+if (process.env.PRIVATE_KEY) {
+  (networks as any).testnet = {
+    url: "https://zetachain-athens.g.allthatnode.com/archive/evm",
+    accounts: [process.env.PRIVATE_KEY as any],
+    chainId: 7001
+  }
+}
+
 const config: HardhatUserConfig = {
   ...getHardhatConfig({ accounts: [process.env.PRIVATE_KEY] }),
-  networks: {
-    hardhat: {
-      accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
-      },
-      chainId: 1337,
-    },
-    testnet: {
-      url: "https://zetachain-athens.g.allthatnode.com/archive/evm",
-      accounts: [process.env.PRIVATE_KEY as any],
-      chainId: 7001
-    }
-  },
+  networks,
   solidity: {
     compilers: [
       {
