c534028c0167a7725d3426561aa45f81345c71cf
skosito <skostic9242@gmail.com>
2025-04-02 18:10:54 +0200

    rename types dir

--- token/wagmi.config.ts
+++ token/wagmi.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@wagmi/cli'
 import { react } from '@wagmi/cli/plugins'
 
 export default defineConfig({
-  out: 'src/generated.ts',
+  out: 'types/index.ts',
   contracts: [
     {
       name: 'ZetaIdxUniversalToken',
