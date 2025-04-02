99e549d6776a3a92a272068cee116098f5aa52c5
skosito <skostic9242@gmail.com>
2025-04-02 19:57:52 +0200

    use real price symbols in idx token

--- token/scripts/localnet.sh
+++ token/scripts/localnet.sh
@@ -48,7 +48,7 @@ pnpm exec hardhat initializeIndex \
   --contract $CONTRACT_ZETACHAIN \
   --tokens "$TOKEN_LIST" \
   --ratios 50,30,20 \
-  --price-symbols "mBTC,mETH,mDOGE" \
+  --price-symbols "BTC,ETH,DOGE" \
   --network localhost
 
 # Get addresses from localnet.json
