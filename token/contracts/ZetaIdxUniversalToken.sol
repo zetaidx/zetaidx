40e8b30ad08d50f5e2ff8c9ff688759c816ab63a
skosito <skostic9242@gmail.com>
2025-04-03 20:20:34 +0200

    read decimals field

--- token/contracts/ZetaIdxUniversalToken.sol
+++ token/contracts/ZetaIdxUniversalToken.sol
@@ -10,7 +10,6 @@ contract ZetaIdxUniversalToken is UniversalToken {
         address token;
         uint256 ratio; // Percentage (1-100)
         string priceSymbol; // symbol used to query price info
-        uint8 decimals; // token decimals
     }
 
     TokenInfo[] public basket;
@@ -40,18 +39,16 @@ contract ZetaIdxUniversalToken is UniversalToken {
         address[] calldata tokens,
         uint256[] calldata ratios,
         string[] calldata priceSymbols
-    ) external onlyOnce onlyOwner {
+    ) external onlyOwner onlyOnce {
         if (tokens.length != ratios.length || tokens.length != priceSymbols.length || tokens.length == 0) revert InvalidInput();
 
         uint256 totalRatio;
         for (uint i = 0; i < tokens.length; i++) {
             if (tokens[i] == address(0) || ratios[i] == 0 || ratios[i] > 100) revert InvalidInput();
-            uint8 decimals = IERC20Metadata(tokens[i]).decimals();
             basket.push(TokenInfo({
                 token: tokens[i],
                 ratio: ratios[i],
-                priceSymbol: priceSymbols[i],
-                decimals: decimals
+                priceSymbol: priceSymbols[i]
             }));
             totalRatio += ratios[i];
         }
@@ -70,10 +67,11 @@ contract ZetaIdxUniversalToken is UniversalToken {
             uint256 tokenAmount = (amount * tokenInfo.ratio) / TOTAL_RATIO;
             
             // Adjust for decimals difference
-            if (tokenInfo.decimals < 18) {
-                tokenAmount = tokenAmount / (10 ** (18 - tokenInfo.decimals));
-            } else if (tokenInfo.decimals > 18) {
-                tokenAmount = tokenAmount * (10 ** (tokenInfo.decimals - 18));
+            uint8 tokenDecimals = IERC20Metadata(tokenInfo.token).decimals();
+            if (tokenDecimals < 18) {
+                tokenAmount = tokenAmount / (10 ** (18 - tokenDecimals));
+            } else if (tokenDecimals > 18) {
+                tokenAmount = tokenAmount * (10 ** (tokenDecimals - 18));
             }
             
             IERC20(tokenInfo.token).transferFrom(msg.sender, address(this), tokenAmount);
@@ -94,10 +92,11 @@ contract ZetaIdxUniversalToken is UniversalToken {
             uint256 tokenAmount = (amount * tokenInfo.ratio) / TOTAL_RATIO;
             
             // Adjust for decimals difference
-            if (tokenInfo.decimals < 18) {
-                tokenAmount = tokenAmount / (10 ** (18 - tokenInfo.decimals));
-            } else if (tokenInfo.decimals > 18) {
-                tokenAmount = tokenAmount * (10 ** (tokenInfo.decimals - 18));
+            uint8 tokenDecimals = IERC20Metadata(tokenInfo.token).decimals();
+            if (tokenDecimals < 18) {
+                tokenAmount = tokenAmount / (10 ** (18 - tokenDecimals));
+            } else if (tokenDecimals > 18) {
+                tokenAmount = tokenAmount * (10 ** (tokenDecimals - 18));
             }
             
             bool success = IERC20(tokenInfo.token).transfer(msg.sender, tokenAmount);
@@ -123,7 +122,7 @@ contract ZetaIdxUniversalToken is UniversalToken {
             tokens[i] = basket[i].token;
             ratios[i] = basket[i].ratio;
             priceSymbols[i] = basket[i].priceSymbol;
-            decimals[i] = basket[i].decimals;
+            decimals[i] = IERC20Metadata(basket[i].token).decimals();
         }
     }
 
@@ -134,7 +133,7 @@ contract ZetaIdxUniversalToken is UniversalToken {
         uint8 decimals
     ) {
         TokenInfo memory t = basket[index];
-        return (t.token, t.ratio, t.priceSymbol, t.decimals);
+        return (t.token, t.ratio, t.priceSymbol, IERC20Metadata(t.token).decimals());
     }
 
     function basketLength() external view returns (uint256) {
